52d41546f019c43270266675b3a09bfe61c1b458
churkinv <churkinv@gmail.com>
2017-12-05 16:06:50 +0200

    Console SelfHost added

---
+++ Gos.SelfHost/Program.cs
@@ -0,0 +1,32 @@
+﻿using Gos.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gos.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ServiceHost host = new ServiceHost(typeof(GosService));
+                host.Open();
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+                host.Close(); // it will stop accepting new calls and complet the ones whis is in process
+            }
+            catch (Exception ex)
+            {
+                // log it
+                Debug.WriteLine(ex.Message);
+            }
+           
+        }
+    }
+}
